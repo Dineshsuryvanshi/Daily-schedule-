fc7d9dd26a9967c604f925b095b4e7776963678d
Dineshsuryvanshi <152321821+Dineshsuryvanshi@users.noreply.github.com>
2025-08-26 07:30:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import os
 import json
 import httpx
 import time
-from dotenv import load_dotenv
-load_dotenv()
 from functools import wraps
 from datetime import datetime, time as dtime
 from typing import List
